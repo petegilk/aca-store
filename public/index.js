b33d5e3520bbd1eeb534a7eb38f4b1af582cef58
peter gilkey <peter_gilkey@yahoo.com>
2020-05-26 20:58:26 -0500

    API fetch, store list of item name, price, and photo

--- public/index.js
+++ public/index.js
@@ -1,2 +1,39 @@
-//stuff
-//more stuff
+'use strict'
+
+window.onload = function () {
+  this.getFetchData();
+}
+
+function getFetchData() {
+  console.log('in fetch function');
+  fetch ('https://my-json-server.typicode.com/jubs16/Products/Products')
+
+  .then (function(response){
+    return response.json();
+  })
+
+  .then(function(data) {
+    console.log(data);
+    printProducts(data);
+  })
+}
+
+let div = document.getElementById('main-container');
+let ul = document.createElement('ul');
+div.appendChild(ul);
+
+function printProducts(arr) {
+  for (let i=0; i< arr.length; i++) {
+    let li = document.createElement('li');
+    li.style.margin = '20px';
+    ul.appendChild(li);
+
+    let img = document.createElement('img');
+    img.setAttribute('src', `${arr[i].imgUrl}`);
+    img.style.width = '150px';
+
+    li.innerHTML = `Product name: ${arr[i].name}<br>Price: ${arr[i].price}<br>`;
+    li.style.fontWeight = '600';
+    li.appendChild(img);
+  }
+}
